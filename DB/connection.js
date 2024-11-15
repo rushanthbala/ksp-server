f30bf288a9568c608012f48ccffe02f7363dba2e
Jathee007 <it21311222@my.sliit.lk>
2024-11-15 13:48:52 +0530

    server error

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { dbUrl } = require("./config");
 
 mongoose
-  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(dbUrl) // No need to specify useNewUrlParser and useUnifiedTopology
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
